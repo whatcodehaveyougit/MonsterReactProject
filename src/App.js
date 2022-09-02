fee7347c339a2e7b8110d607c52f3fdfde35cccb
whatcodehaveyougit <siggywatt@yahoo.com>
2022-09-02 16:25:06 +0200

    names updating on click

--- src/App.js
+++ src/App.js
@@ -4,22 +4,27 @@ import { Component } from 'react';
 
 class App extends Component {
 
+
+  constructor() {
+    super()
+    this.state = {
+      name: { first: "Sigurd", last: "Watt" }
+    }
+
+
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reloadddd.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>My name is: { this.state.name.first } { this.state.name.last }</p>
+
+          {/* <button onClick={() => { this.setState({ name: "Jim" }) } } >Change Name</button> */}
+          {/* This line creates a new object in the state so that the DOM re-renders and updates */}
+
+          <button onClick={() => { this.setState({name:{ first: "Dig", last: "Busby" }}) } } >Change Name</button>
         </header>
       </div>
     );
